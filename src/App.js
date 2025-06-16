46c7a8078307ad6c3ae4cc6bdd2a0416db50c42d
N.E.T <143912452+TarruckWheeler@users.noreply.github.com>
2025-06-16 13:34:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,3 @@
-/**
- * BRIDGES UNESCO 3.0 - Main App Component
- * Created by: Tarruck Wheeler (tarruck@stanford.edu)
- * Institution: Stanford International Policy & Governance
- * Program: UNESCO Intercultural Leadership Program
- * Focus: South Florida Refugee Community Integration
- * 
- * This application facilitates intercultural dialogue and community integration
- * for refugee communities in South Florida through UNESCO-approved methodologies.
- */
-
 import BridgesUNESCO from './components/BridgesUNESCO.jsx'
 import './App.css'
 
