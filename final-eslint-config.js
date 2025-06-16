595e8af8eefc918fd9687023a95a39d588e626d0
N.E.T <143912452+TarruckWheeler@users.noreply.github.com>
2025-06-16 09:31:54 -0400

    Add files via upload

---
+++ final-eslint-config.js
@@ -0,0 +1,27 @@
+// BRIDGES UNESCO 3.0 - Created by Tarruck Wheeler (tarruck@stanford.edu)
+// ESLint Configuration for UNESCO Intercultural Leadership Platform
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+  },
+}
