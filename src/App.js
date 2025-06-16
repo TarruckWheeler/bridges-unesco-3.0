94d8ce9f54d34f2efa974bf69da2f49f99799920
N.E.T <143912452+TarruckWheeler@users.noreply.github.com>
2025-06-16 13:23:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,12 @@
  * for refugee communities in South Florida through UNESCO-approved methodologies.
  */
 
-import BridgesUNESCO from './components/BridgesUNESCO'
+import BridgesUNESCO from './components/BridgesUNESCO.jsx'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      {/* UNESCO Platform - Created by Tarruck Wheeler */}
       <BridgesUNESCO />
     </div>
   )
