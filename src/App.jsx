e1c77ce2514c27942d18c0115e5f6b0932d5f7b0
N.E.T <143912452+TarruckWheeler@users.noreply.github.com>
2025-06-16 14:58:17 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+/**
+ * BRIDGES UNESCO 3.0 - Main App Component
+ * Created by: Tarruck Wheeler (tarruck@stanford.edu)
+ * Institution: Stanford International Policy & Governance
+ * Program: UNESCO Intercultural Leadership Program
+ * Focus: South Florida Refugee Community Integration
+ */
+
+import BridgesUNESCO from './components/BridgesUNESCO'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <BridgesUNESCO />
+    </div>
+  )
+}
+
+export default App
